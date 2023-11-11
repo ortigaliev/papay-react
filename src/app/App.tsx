a863d667e083187bc813f59982d07df39df7edff
ortigaliev <ortigaliyevm@gmail.com>
2023-11-11 13:24:18 +0900

    feat: standart integration of MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,23 @@
+
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 import '../css/App.css';
+import { Box, Stack, Button} from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is frontend </h1>
-    </div>
+  <Container maxWidth={'sm'}>
+    <Stack flexDirection={'column'}>
+      <Box sx={{my: 4}}>
+        <Typography variant='h1' component={'h1'} gutterBottom>
+          Create React app on TS with Redux
+        </Typography>
+      </Box>
+      <Button variant="contained">Contained</Button>
+    </Stack>
+
+  </Container>
   );
 }
 
