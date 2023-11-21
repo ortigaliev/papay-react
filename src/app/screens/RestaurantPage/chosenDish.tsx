e9c8b462c4a4d7f9e8ac041c2911546e99f3d478
ortigaliev <ortigaliyevm@gmail.com>
2023-11-22 00:45:13 +0900

    featL: chosenDish page publishing

--- src/app/screens/RestaurantPage/chosenDish.tsx
+++ src/app/screens/RestaurantPage/chosenDish.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+
+import Swiper, { FreeMode, Navigation, Thumbs } from "swiper";
+import { Container, Stack } from "@mui/material";
+import {Favorite, FavoriteBorder} from "@mui/icons-material"
+import Checkbox from "@mui/material/Checkbox";
+
+const chosen_list = Array.from(Array(3).keys());
 
 export function ChosenDish() {
-    return <div>ChosenDish</div>
-}
+  const label = { inputProps: { "aria-label": "Checkbox demo"} };
+    return (
+      <div className="chosen_dish_page">
+        <Container className="dish_container">
+          <Stack className="chosen_dish_slider">
+            <Swiper
+            className="dish_swiper"
+            loop={true}
+            navigation={true}
+            spaceBetween={10}
+              navigation={true}
+
+              modules={[FreeMode, Navigation, Thumbs]}
+            >
+
+              { chosen_list.map((ele) => {
+                const image_path =`others/sandivich.jpg`;
+
+              })}
+             </Swiper>
+          </Stack>
+
+        </Container>
+      </div>
+
+    )
+  };
